8493430379a7e0b51cd5e59d5379f4d9dc9809fc
anavallejorivera <avallejorivera360@gmail.com>
2026-04-20 18:08:23 +0200

    agregar archivos

---
+++ src/ejemplo.js
@@ -0,0 +1,13 @@
+
+     questions: [
+            {"q": "AWT:", "options": ["Librería anterior creación", "Mismo nivel Swing", "Evolución Swing", "Ninguna"], "correct": 0, "explanation": "La respuesta correcta es A."},
+            {"q": "FlowLayout coloca:", "options": ["BorderLayout", "CardLayout", "FlowLayout", "GridLayout"], "correct": 2, "explanation": "La respuesta correcta es C."},
+            {"q": "Instalar WindowBuilder si:", "options": ["NetBeans módulo", "Eclipse módulo", "MySQL módulo", "Ninguna"], "correct": 1, "explanation": "La respuesta correcta es B."},
+            {"q": "Listener es:", "options": ["Driver", "Listener", "Writer", "Todas incorrectas"], "correct": 1, "explanation": "La respuesta correcta es B."},
+            {"q": "GridBagLayout:", "options": ["No existe", "AWT", "Swing", "Ambos"], "correct": 2, "explanation": "La respuesta correcta es C."},
+            {"q": "Componentes letra J:", "options": ["Instalación AWT Swing", "Incorporación código", "Prueba visibles", "Ninguna correcta"], "correct": 3, "explanation": "La respuesta correcta es D."},
+            {"q": "Eventos Swing diferencian:", "options": ["J inicial", "Sin listener", "Pestaña Design", "Ninguna correcta"], "correct": 3, "explanation": "La respuesta correcta es D."},
+            {"q": "MotifLookAndFeel es:", "options": ["Layout", "Componente", "Estilo controles", "Ninguna"], "correct": 2, "explanation": "La respuesta correcta es C."},
+            {"q": "No es tipo control menús:", "options": ["MenuBar", "ShortMenuCut", "MenuItem", "PopupMenu"], "correct": 1, "explanation": "La respuesta correcta es B."},
+            {"q": "Interfaz gráfica permite:", "options": ["Listener", "Drag and drop", "JTree", "Ninguna"], "correct": 1, "explanation": "La respuesta correcta es B."}
+          ] 
